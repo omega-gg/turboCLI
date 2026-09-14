538e7e8a1ac9a79b99dced3a7c07a2f946258ac1
Benjamin Arnaud <bunjee@omega.gg>
2026-09-14 16:26:36 +0200

    turboCLI 9ca68310ae2e98295bc732095922604664e3c0bb

--- bash/turbo/build.sh
+++ bash/turbo/build.sh
@@ -32,7 +32,7 @@ repository="https://github.com/omega-gg/turboCLI.git"
 
 repository_offloader="https://github.com/omega-gg/turbo-offloader.git"
 
-commit="b847874f9b030dc1c811d13dd9fa8f9b51200b6c" # Also update in check.sh
+commit="9ca68310ae2e98295bc732095922604664e3c0bb" # Also update in check.sh
 
 commit_offloader="7e80cdfc6dfd311cf2274b4c3a4140184e696ce9"
 
